c28080d9ac4cf2f4ec0ae5c54573a688223dafe2
Akshit Agarwal <akshitagarwal10@gmail.com>
2017-03-05 03:10:41 +0530

    BoxShooter

---
+++ Assets/Scripts/GunTriggerAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunTriggerAnimation : MonoBehaviour {
+	Animator anim;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.Mouse0)) {
+			anim.SetTrigger ("fire");
+		}
+	}
+}
